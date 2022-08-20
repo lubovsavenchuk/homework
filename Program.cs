cb5f5d603cc6bf18f3f26b64aa25e7a8987d3a25
Савенчук Любовь <sav_lu@gmail.com>
2022-08-20 22:53:03 +0500

    first task

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+// a = 5; b = 7 -> max = 7
+// a = 2 b = 10 -> max = 10
+// a = -9 b = -3 -> max = -3
+
+Console.WriteLine("Type first number here: ");
+string num1 = Console.ReadLine();
+int a = Int32.Parse(num1);
+
+Console.WriteLine("Type second number here: ");
+string num2 = Console.ReadLine();
+int b = Int32.Parse(num2);
+int max = a;
+int min;
+
+if (b > max) max = b; min = a;
+
+Console.Write("max = ");
+Console.WriteLine(max);
+Console.Write("min = ");
+Console.WriteLine(min);
+
--- README.md
+++
@@ -1,8 +0,0 @@
--hello, developer!
- have some news to share
-
-
--hi! nice to hear from you. what are the news?
-
-
--Michael said the Inthernet would blown up one day!
