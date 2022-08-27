522c29562fdaf0ef51a8a20edfa6a5c8718d5282
Савенчук Любовь <sav_lu@gmail.com>
2022-08-27 12:19:05 +0500

    ex8_refresh

--- задача_8/Program.cs
+++ задача_8/Program.cs
@@ -1,23 +1,17 @@
 ﻿// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
-//5 -> 2, 4
-//8 -> 2, 4, 6, 8
-//  Идея решения задачи такая, код выдает ошибку. Но я уже иссякла и не знаю, как их исправлять. Если поможете разобраться, буду безмерно благодарно, так как очень интересно!
 
 Console.WriteLine("Type a number: ");
-int N = Console.ReadLine();
+string N = Console.ReadLine();
 int z = Int32.Parse(N);
 int index = 0;
 int EvenNumbers = 0;
 
-int n = array.Length;
-int[] array = new int [1, N];
-while (index < n)
+while (z > 1)
 {
-   if (array [index] % 2 == 0)
+   if ( index % 2 == 0)
    {
-      Console.WriteLine(EvenNumbers);
       index++;
    }
-EvenNumbers++;
+
 }
-Console.WriteLine(EvenNumbers);
+Console.WriteLine(index);
